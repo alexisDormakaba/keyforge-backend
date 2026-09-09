cfcc9d7117a0c5c596aef56b04eb8e3de7ffcfac
alexisDormakaba <alexis.ortega@dormakaba.com>
2026-09-09 15:05:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/health")
+def health():
+    return {
+        "healthy": True
+    }
