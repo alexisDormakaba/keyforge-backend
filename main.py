699e294abf35e3931c322a13b2e4a1bf75cfcea2
alexisDormakaba <alexis.ortega@dormakaba.com>
2026-09-10 08:15:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ImageRequest(BaseModel):
+    image_url: str
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-@app.get("/health")
-def health():
+@app.post("/test-image")
+def test_image(data: ImageRequest):
     return {
-        "healthy": True
+        "received_url": data.image_url
     }
