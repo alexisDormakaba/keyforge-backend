5b0141a804e5ae695349e21a25743767fb5736e7
alexisDormakaba <alexis.ortega@dormakaba.com>
2026-09-10 08:50:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
 import requests
+from PIL import Image
+from io import BytesIO
+
+import torch
+import open_clip
 
 app = FastAPI()
 
+model, _, preprocess = open_clip.create_model_and_transforms(
+    "ViT-B-32",
+    pretrained="laion2b_s34b_b79k"
+)
+
+tokenizer = open_clip.get_tokenizer("ViT-B-32")
+
 class ImageRequest(BaseModel):
     image_url: str
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-@app.post("/analyze-image")
-def analyze_image(data: ImageRequest):
+
+@app.post("/generate-embedding")
+def generate_embedding(data: ImageRequest):
 
     response = requests.get(data.image_url)
 
+    image = Image.open(
+        BytesIO(response.content)
+    ).convert("RGB")
+
+    image_tensor = preprocess(image).unsqueeze(0)
+
+    with torch.no_grad():
+        embedding = model.encode_image(image_tensor)
+
+    vector = embedding[0].cpu().numpy().tolist()
+
     return {
-        "status_code": response.status_code,
-        "size_bytes": len(response.content)
+        "dimensions": len(vector),
+        "embedding": vector[:10]
     }
