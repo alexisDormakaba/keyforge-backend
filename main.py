976c8c414ae2e9a34b225d55862aec3c122d6380
alexisDormakaba <alexis.ortega@dormakaba.com>
2026-09-11 09:35:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URLs")
 SUPABASE_KEY = os.getenv("SUPABASE_PRIMARY_KEY")
 
 supabase = create_client(
-    SUPABASE_URL,
+    SUPABASE_URLs,
     SUPABASE_KEY
 )
 
